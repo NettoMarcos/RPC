168446e7e71ec32d83898eacebec9baf039b7163
Marcos Netto <104269291+NettoMarcos@users.noreply.github.com>
2024-05-20 19:21:10 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -5,9 +5,12 @@ def main():
     calculadora = Pyro4.Proxy("PYRO:obj_369e8b0f96954353a3b166bc579639db@localhost:53649")  # Substitua 'obj_123456' pelo URI do servidor
     soma = calculadora.somar(85, 3)
     sub = calculadora.subtracao(58,6)
+    multi = calculadora.multiplicacao(5, 9)
     print("A soma é: ", soma)
     print()
-    print("A subtracao é: ", sub)
+    print("A subtração é: ", sub)
+    print()
+    print("A multiplicação é: ", multi)
 
 
 if __name__ == "__main__":
